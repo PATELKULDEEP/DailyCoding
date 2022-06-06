d389680f87413f83651ebedd15d3e82938f48586
KULDEEP PATEL <patelkuldeep0001@gmail.com>
2022-06-06 23:50:32 +0530

    Addition Operation Start Using LinkedList

---
+++ DailyCoding/src/AddOperationLinkedList.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AddOperationLinkedList {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int n1 = input.nextInt();
+        int n2 = input.nextInt();
+        
+    }
+
+}
