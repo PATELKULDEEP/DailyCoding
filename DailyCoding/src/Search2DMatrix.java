4758389e59619ab6b8cf2d0892d365cbf7310b81
KULDEEP PATEL <patelkuldeep0001@gmail.com>
2022-07-24 22:44:37 +0530

    Search 2D matrix

--- DailyCoding/src/Search2DMatrix.java
+++ DailyCoding/src/Search2DMatrix.java
@@ -4,7 +4,7 @@ public class Search2DMatrix {
        int[][] matrix =  {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
        int target = 5;
 
-       System.out.println(searchMatrix(matrix,target));
+       System.out.println("Target Found - " + searchMatrix(matrix,target));
     }
     public static boolean searchMatrix(int[][] matrix, int target) {
 
